a869658e20638be3f7a48e1096866768656760a3
nickyleo <momononoa100100@gmail.com>
2019-09-19 00:22:22 +0700

    up

--- ionic-beer/src/app/app-routing.module.ts
+++ ionic-beer/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  { path: 'beer', loadChildren: './beer/beer.module#BeerPageModule' }
 ];
 @NgModule({
   imports: [
---
+++ ionic-beer/src/app/beer/beer.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-beer',
+  templateUrl: './beer.page.html',
+  styleUrls: ['./beer.page.scss'],
+})
+export class BeerPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
